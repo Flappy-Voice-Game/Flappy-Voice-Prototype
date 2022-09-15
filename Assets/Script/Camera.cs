aca0b66cda57956fb08422fb62fab22caf5d57dc
sl0vvman <113129226+sl0vvman@users.noreply.github.com>
2022-09-15 09:02:05 -0700

    Add project

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     [SerializeField] Transform player;
-    void Start()
-    {
 
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x + 1, 1.22f, -10f);
+        transform.position = new Vector3(player.transform.position.x + 1, 1.22f, -10f); //слежка камеры по горизонтале
 
     }
 }
--- Assets/Script/GeneratorTrub.cs
+++ Assets/Script/GeneratorTrub.cs
@@ -6,43 +6,46 @@ public class GeneratorTrub : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject pipes;
+    public GameObject pipesMones;
     public GameObject fon;
+
     [SerializeField] float rastTrub=0;
     [SerializeField] float rastfona = 0;
     void Start()
     {
-        StartCoroutine(Spawner());
+        StartCoroutine(Spawner()); //запуск карутин
         StartCoroutine(SpawnerFona());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     IEnumerator Spawner()
     {
         while (true)
         {
-            
-            yield return new WaitForSeconds(2);
+            float randTruba = Random.Range(1,10); //рандомятся высота труб и монетки
+            yield return new WaitForSeconds(1);
             float rand = Random.Range(-1.87f, -0.25f);
-            GameObject newPipe = Instantiate(pipes, new Vector3(rastTrub, rand, 1), Quaternion.identity);
-            rastTrub += 5;
-            Destroy(newPipe, 20);
+
+            if (randTruba > 5) // спаун трубы без монетки
+            {                
+                GameObject newPipe = Instantiate(pipes, new Vector3(rastTrub, rand, 1), Quaternion.identity);
+                rastTrub += 5;
+            }
+            else // спаун трубы с монеткой
+            {
+                
+                GameObject newPipe = Instantiate(pipesMones, new Vector3(rastTrub, rand, 1), Quaternion.identity);
+                rastTrub += 5;
+            }       
         }
         
     }
-    IEnumerator SpawnerFona()
+    IEnumerator SpawnerFona() //спаун фона
     {
         while (true)
         {
-
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1);
             GameObject newPipe = Instantiate(fon, new Vector3(rastfona, 0.57f, 0), Quaternion.identity);
             rastfona += 2.85f;
-            Destroy(newPipe, 20);
         }
-
     }
 }
